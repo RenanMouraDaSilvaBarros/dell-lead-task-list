bf9ce2b57f02f901793269e1136235e8cdd35383
renanmoura6941 <naneraruom@gmail.com>
2021-03-14 12:51:17 -0300

    texfild

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,9 @@
-import 'package:dell_lead_task_list/page/task_list_screen.dart';
+import 'package:dell_lead_task_list/page/task_list_content_screen.dart';
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(MaterialApp(
-    home: TaskListScreen(),
+    home: TaskListContentScreen(),
   ));
 }
 
--- lib/page/task_list_content_screen.dart
+++ lib/page/task_list_content_screen.dart
@@ -1,10 +1,28 @@
 import 'package:flutter/material.dart';
 
-class TaskListContent extends StatelessWidget {
+class TaskListContentScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return Scaffold(
+      appBar: AppBar(),
+      body: Container(
+        padding: EdgeInsets.symmetric(horizontal: 10),
+        child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+          TextField(
+            decoration: InputDecoration(
+              hintText: "Tarefa",
+              border: OutlineInputBorder(
+                borderSide: new BorderSide(color: Colors.black, width: 10),
+              ),
+            ),
+          ),
+          FlatButton(
+            color: Colors.blue,
+            onPressed: () {},
+            child: Text("Adicionar tarefa"),
+          )
+        ]),
+      ),
     );
   }
 }

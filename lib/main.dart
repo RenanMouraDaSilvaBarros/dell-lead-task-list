bc51165fb604e713e9d1a1a0be0560a86552bed3
renanmoura6941 <naneraruom@gmail.com>
2021-03-14 11:03:17 -0300

    struct project

---
+++ lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:dell_lead_task_list/task_list/page/task_list_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: TaskListScreen(),
+  ));
+}
+
